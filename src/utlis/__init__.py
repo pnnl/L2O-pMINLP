7ea64e905e13c4619670312a343c369b33ff267a
Bo Tang <botang@mie.utoronto.ca>
2024-04-08 21:27:41 -0400

    Add nmTest

--- src/utlis/__init__.py
+++ src/utlis/__init__.py
@@ -1,2 +1,2 @@
-from src.utlis.solve_test import msSolveTest
+from src.utlis.solve_test import msSolveTest, nmSolveTest
 from src.utlis.data import dataSplit
--- src/utlis/solve_test.py
+++ src/utlis/solve_test.py
@@ -21,3 +21,31 @@ def msSolveTest(model, tee=False):
     print("Elapsed Time: {:.4f} sec".format(tock - tick))
     print()
     return solvals, objval
+
+
+def nmSolveTest(problem, datapoint, model):
+    """
+    Test function for neuroMANCER
+    """
+    # inference
+    problem.eval()
+    tick = time.time()
+    output = problem(datapoint)
+    key_name = datapoint["name"] + "_" + list(problem.vars.keys())[0]
+    x = output[key_name]
+    tock = time.time()
+    # get values
+    for ind in model.vars["x"]:
+        model.vars["x"][ind].value = x[0, ind].item()
+    solvals, objval = model.getVal()
+    # results
+    print("Binary Variables:", model.binInd)
+    print("Integer Variables:", model.intInd)
+    for k, v in solvals.items():
+        for i, val in v.items():
+            print("{}[{}]: {:.2f}".format(k, i, val), end=" ")
+    print("\nObjective Value: {:.2f}".format(objval))
+    print("Constraint Violations: {:.4f}".format(sum(model.calViolation())))
+    print("Elapsed Time: {:.4f} sec".format(tock - tick))
+    print()
+    return solvals, objval
