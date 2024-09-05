d729f267f9b0010288988e1a3a1e129b7f37f598
Bo Tang <botang@mie.utoronto.ca>
2024-09-05 01:20:28 -0400

    Bug fixed: key

--- src/utlis/solve_test.py
+++ src/utlis/solve_test.py
@@ -36,13 +36,13 @@ def nm_test_solve(var_key, components, datapoint, model):
             datapoint.update(comp(datapoint))
     tock = time.time()
     # get values
-    for i in model.vars[var_key]:
-        model.vars[var_key][i].value = datapoint[var_key][0, i].item()
+    for i in model.vars["x"]:
+        model.vars["x"][i].value = datapoint[var_key][0, i].item()
     solvals, objval = model.get_val()
     # results
     print("Binary Variables:", model.bin_ind)
     print("Integer Variables:", model.int_ind)
-    for i, val in solvals[var_key].items():
+    for i, val in solvals["x"].items():
         print("{}[{}]: {:.2f}".format(var_key, i, val), end=" ")
     print("\nObjective Value: {:.2f}".format(objval))
     print("Constraint Violations: {:.4f}".format(sum(model.cal_violation())))
