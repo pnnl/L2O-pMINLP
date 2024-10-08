aac1230f6707db6037a37c8a05f59d6f0849b3d5
Bo Tang <botang@mie.utoronto.ca>
2024-10-08 15:12:17 -0400

    Add MILP

--- src/problem/__init__.py
+++ src/problem/__init__.py
@@ -1,3 +1,5 @@
+from src.problem.math_solver import linear as msLinear
+from src.problem.neuromancer import linear as nmLinear
 from src.problem.math_solver import quadratic as msQuadratic
 from src.problem.neuromancer import quadratic as nmQuadratic
 from src.problem.math_solver import nonconvex as msNonconvex
@@ -5,4 +7,4 @@ from src.problem.neuromancer import nonconvex as nmNonconvex
 from src.problem.math_solver import rosenbrock as msRosenbrock
 from src.problem.neuromancer import rosenbrock as nmRosenbrock
 from src.problem.math_solver import ackley as msAckley
-from src.problem.neuromancer import ackley as nmAckley 
+from src.problem.neuromancer import ackley as nmAckley
