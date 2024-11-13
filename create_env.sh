ae9f36836a2d1056c7f9623f401219acda63c979
Bo Tang <botang@mie.utoronto.ca>
2024-11-13 01:04:45 -0500

    Update create_env.sh

--- create_env.sh
+++ create_env.sh
@@ -5,23 +5,24 @@
 
 # global vars
 VENVS_DIR="PATH_TO_VENV"
-VENV_NAME="neuromancer"
-PYTHON_VER="3.9"
-GRB_VER="10.0.3"
+VENV_NAME="solver"
+PYTHON_VER="3.10.13"
+CUDA_VER="12.2"
+SCIP_VER="9.0.0"
+IPOPT_VER="3.14.14"
+GRB_VER="11.0.1"
+TORCH_VER="2.5.0"
+NM_VER="1.5.2"
 LOGDIR="./logs"
 
 # load module
 echo "Load module..."
 module purge
-module load cuda
+module load StdEnv/2023
+module load gcc/12.3
 module load python/$PYTHON_VER
-module load gurobi/$GRB_VER
-module load scip
-module load ipopt
-module load rust
-# check if the license is set
-gurobi_cl 1> /dev/null && echo Success || echo Fail
-echo ""
+module load ipopt/$IPOPT_VER
+module load scipoptsuite/$SCIP_VER
 
 # create virtual env
 if [ ! -d "./$VENVS_DIR/$VENV_NAME" ]; then
@@ -32,28 +33,22 @@ if [ ! -d "./$VENVS_DIR/$VENV_NAME" ]; then
   echo ""
 
   echo "Install requirements..."
-
-  echo "  Install NeuroMANCER..."
-  pip install neuromancer
-
-  # install gurobipy
-  echo "  Install GurobiPy..."
-  cp -r $GUROBI_HOME/ .
-  cd $GRB_VER
-  python setup.py install
-  cd ..
-  rm -r $GRB_VER
-
-  # pip install
-  echo "  Install requirements..."
   pip install --no-index --upgrade pip
   pip install tqdm
-  pip install numpy
+  pip install numpy==1.22.4+computecanada
+  pip install oldest-supported-numpy
   pip install pandas
   pip install Pyomo
-  pip install --no-index torch torchvision torchtext torchaudio
-
-  echo "  Install submitit..."
+  pip install --no-index torch==$TORCH_VER torchvision torchtext torchaudio
+  pip install scipy matplotlib scikit-learn networkx==3.0
+  pip install dill pydot==1.4.2 pyts numba plum-dispatch==1.7.3
+  pip install graphviz torchsde
+  pip install mlflow==2.5.0 --ignore-installed
+  pip install opentelemetry-api opentelemetry-sdk sqlparse cachetools
+  pip install cvxpy cvxopt casadi cvxpylayers
+  pip install torchdiffeq toml
+  pip install lightning wandb
+  pip install neuromancer==$NM_VER --no-deps
   pip install submitit
 
 # activate virtual env
