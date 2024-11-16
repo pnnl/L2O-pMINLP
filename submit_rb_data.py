f07439e6a405b9a36934bbf0075eedc899aec080
Bo Tang <botang@mie.utoronto.ca>
2024-11-16 15:14:10 -0500

    Bug fixe: data generate

--- submit_rb_data.py
+++ submit_rb_data.py
@@ -36,22 +36,7 @@ config.lr = 1e-3                        # learning rate
 config.penalty = 100                    # penalty weight
 config.penalty = 100
 
-# different sample size
-for sample in [800, 80000]:
-    config.samples = sample
-    # random seed
-    random.seed(42)
-    np.random.seed(42)
-    torch.manual_seed(42)
-    torch.cuda.manual_seed(42)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    # init problem
-    config.steepness = 50            # steepness factor
-    num_blocks = config.size         # number of blocks
-    train_size = config.samples      # number of train
-    test_size = 100                  # number of test size
-    val_size = 1000                  # number of validation size
+def run_rndCls(config):
     # parameters as input data
     p_low, p_high = 1.0, 8.0
     a_low, a_high = 0.5, 4.5
@@ -72,10 +57,77 @@ for sample in [800, 80000]:
     loader_train = DataLoader(data_train, batch_size, num_workers=0, collate_fn=data_train.collate_fn, shuffle=True)
     loader_test = DataLoader(data_test, batch_size, num_workers=0, collate_fn=data_test.collate_fn, shuffle=False)
     loader_val = DataLoader(data_val, batch_size, num_workers=0, collate_fn=data_val.collate_fn, shuffle=True)
+    # run
+    run.rosenbrock.rndCls(loader_train, loader_test, loader_val, config)
+
+def run_rndThd(config):
+    # parameters as input data
+    p_low, p_high = 1.0, 8.0
+    a_low, a_high = 0.5, 4.5
+    p_train = np.random.uniform(p_low, p_high, (train_size, 1)).astype(np.float32)
+    p_test  = np.random.uniform(p_low, p_high, (test_size, 1)).astype(np.float32)
+    p_val   = np.random.uniform(p_low, p_high, (val_size, 1)).astype(np.float32)
+    a_train = np.random.uniform(a_low, a_high, (train_size, num_blocks)).astype(np.float32)
+    a_test  = np.random.uniform(a_low, a_high, (test_size, num_blocks)).astype(np.float32)
+    a_val   = np.random.uniform(a_low, a_high, (val_size, num_blocks)).astype(np.float32)
+    # nm datasets
+    from neuromancer.dataset import DictDataset
+    data_train = DictDataset({"p":p_train, "a":a_train}, name="train")
+    data_test = DictDataset({"p":p_test, "a":a_test}, name="test")
+    data_val = DictDataset({"p":p_val, "a":a_val}, name="dev")
+    # torch dataloaders
+    from torch.utils.data import DataLoader
+    batch_size = 64
+    loader_train = DataLoader(data_train, batch_size, num_workers=0, collate_fn=data_train.collate_fn, shuffle=True)
+    loader_test = DataLoader(data_test, batch_size, num_workers=0, collate_fn=data_test.collate_fn, shuffle=False)
+    loader_val = DataLoader(data_val, batch_size, num_workers=0, collate_fn=data_val.collate_fn, shuffle=True)
+    # run
+    run.rosenbrock.rndThd(loader_train, loader_test, loader_val, config)
+
+def run_rndSte(config):
+    # parameters as input data
+    p_low, p_high = 1.0, 8.0
+    a_low, a_high = 0.5, 4.5
+    p_train = np.random.uniform(p_low, p_high, (train_size, 1)).astype(np.float32)
+    p_test  = np.random.uniform(p_low, p_high, (test_size, 1)).astype(np.float32)
+    p_val   = np.random.uniform(p_low, p_high, (val_size, 1)).astype(np.float32)
+    a_train = np.random.uniform(a_low, a_high, (train_size, num_blocks)).astype(np.float32)
+    a_test  = np.random.uniform(a_low, a_high, (test_size, num_blocks)).astype(np.float32)
+    a_val   = np.random.uniform(a_low, a_high, (val_size, num_blocks)).astype(np.float32)
+    # nm datasets
+    from neuromancer.dataset import DictDataset
+    data_train = DictDataset({"p":p_train, "a":a_train}, name="train")
+    data_test = DictDataset({"p":p_test, "a":a_test}, name="test")
+    data_val = DictDataset({"p":p_val, "a":a_val}, name="dev")
+    # torch dataloaders
+    from torch.utils.data import DataLoader
+    batch_size = 64
+    loader_train = DataLoader(data_train, batch_size, num_workers=0, collate_fn=data_train.collate_fn, shuffle=True)
+    loader_test = DataLoader(data_test, batch_size, num_workers=0, collate_fn=data_test.collate_fn, shuffle=False)
+    loader_val = DataLoader(data_val, batch_size, num_workers=0, collate_fn=data_val.collate_fn, shuffle=True)
+    # run
+    run.rosenbrock.rndSte(loader_train, loader_test, loader_val, config)
+
+# different sample size
+for sample in [800, 80000]:
+    config.samples = sample
+    # random seed
+    random.seed(42)
+    np.random.seed(42)
+    torch.manual_seed(42)
+    torch.cuda.manual_seed(42)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    # init problem
+    config.steepness = 50            # steepness factor
+    num_blocks = config.size         # number of blocks
+    train_size = config.samples      # number of train
+    test_size = 100                  # number of test size
+    val_size = 1000                  # number of validation size
     # submit experiments
-    #run.rosenbrock.rndCls(loader_train, loader_test, loader_val, config)
-    #run.rosenbrock.rndThd(loader_train, loader_test, loader_val, config)
-    #run.rosenbrock.rndSte(loader_train, loader_test, loader_val, config)
+    #run_rndCls(config)
+    #run_rndThd(config)
+    #run_rndSte(config)
     # rounding classification
     executor = submitit.AutoExecutor(folder="logs")
     executor.update_parameters(slurm_additional_parameters={"account": "def-khalile2",
@@ -84,7 +136,7 @@ for sample in [800, 80000]:
                                mem_gb=64,
                                cpus_per_task=16,
                                gpus_per_node=1)
-    job = executor.submit(run.rosenbrock.rndCls, loader_train, loader_test, loader_val, config)
+    job = executor.submit(run_rndCls, config)
     print(f"Submitted job with ID: {job.job_id}")
     # learnable threshold
     executor = submitit.AutoExecutor(folder="logs")
@@ -94,7 +146,7 @@ for sample in [800, 80000]:
                                mem_gb=64,
                                cpus_per_task=16,
                                gpus_per_node=1)
-    job = executor.submit(run.rosenbrock.rndThd, loader_train, loader_test, loader_val, config)
+    job = executor.submit(run_rndThd, config)
     print(f"Submitted job with ID: {job.job_id}")
     # STE Rounding
     executor = submitit.AutoExecutor(folder="logs")
@@ -104,5 +156,5 @@ for sample in [800, 80000]:
                                mem_gb=64,
                                cpus_per_task=16,
                                gpus_per_node=1)
-    job = executor.submit(run.rosenbrock.rndSte, loader_train, loader_test, loader_val, config)
+    job = executor.submit(run_rndSte, config)
     print(f"Submitted job with ID: {job.job_id}")
