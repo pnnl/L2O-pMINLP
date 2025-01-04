f7eae8e38d742045e0933e03ca54b4f8e6afdcf5
Bo Tang <botang@mie.utoronto.ca>
2025-01-03 21:18:07 -0500

    Bug fixed: index

--- src/func/linsys.py
+++ src/func/linsys.py
@@ -17,8 +17,8 @@ class completePartial(nn.Module):
         self.other_ind = [i for i in range(num_var) if i not in partial_ind]
         # necessary computation
         self.A = torch.from_numpy(A).float()
-        self._A_partial = self.A[:, :num_var//2]
-        self._A_other_inv = torch.inverse(self.A[:, num_var//2:])
+        self._A_partial = self.A[:, self.partial_ind]
+        self._A_other_inv = torch.inverse(self.A[:, self.other_ind])
         # keys
         self.x_key = var_key
         self.b_key = rhs_key
@@ -32,7 +32,7 @@ class completePartial(nn.Module):
         x, b = data[self.x_key], data[self.b_key]
         # complete vars
         x_comp = torch.zeros(x.shape[0], self.num_var, device=x.device)
-        x_comp[:, :self.num_var//2] = x
-        x_comp[:, self.num_var//2:] = (b - x @ self._A_partial.T) @ self._A_other_inv.T
+        x_comp[:, self.partial_ind] = x
+        x_comp[:, self.other_ind] = (b - x @ self._A_partial.T) @ self._A_other_inv.T
         data[self.out_key] = x_comp
         return data
